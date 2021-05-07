3064a476a028d81b03af65135b78f329c7c48723
Thiago Gorgulho <thiagogorgulho@outlook.com>
2021-05-07 20:46:10 -0300

    Player added

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 mod map;
+mod player;
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
 struct State {
-    map: Map
+    map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
         Self {
-            map: Map::new()
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
         }
     }
 }
@@ -23,7 +27,9 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
--- src/map.rs
+++ src/map.rs
@@ -31,13 +31,29 @@ impl Map {
 
                 match self.tiles[index] {
                     TileType::Floor => {
-                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                        ctx.set(x, y, GRAY12, BLACK, to_cp437('.'));
                     }
                     TileType::Wall => {
                         ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
                     }
                 }
             }
+        }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_index(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_map_index(&self, point: Point) -> Option<usize> {
+        if (!self.in_bounds(point)) {
+            None
+        } else {
+            Some(map_index(point.x, point.y))
         }
     }
 }
