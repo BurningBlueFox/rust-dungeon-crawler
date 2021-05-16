e72b3332624780a4b4214623eaeb40cc14a8e119
Thiago Gorgulho <thiagogorgulho@outlook.com>
2021-05-15 22:16:53 -0300

    Enemies have random movement

--- src/components.rs
+++ src/components.rs
@@ -1,9 +1,9 @@
 pub use crate::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Render{
-    pub color : ColorPair,
-    pub glyph : FontCharType
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -11,3 +11,6 @@ pub struct Player;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct MovingRandomly;
