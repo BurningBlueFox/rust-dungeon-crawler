ddc640321efebf08d66fcb6b85b62dcbc90e20bf
Thiago Gorgulho <thiagogorgulho@outlook.com>
2021-05-27 22:58:02 -0300

    Victory when reach the objective

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,7 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
         map_builder
             .rooms
             .iter()
@@ -84,23 +85,49 @@ impl State {
         ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play again.");
 
         if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.ecs = World::default();
-            self.resources = Resources::default();
-            let mut rng = RandomNumberGenerator::new();
-            let map_builder = MapBuilder::new(&mut rng);
-
-            spawn_player(&mut self.ecs, map_builder.player_start);
-            spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
-            map_builder
-                .rooms
-                .iter()
-                .skip(1)
-                .map(|r| r.center())
-                .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
-            self.resources.insert(map_builder.map);
-            self.resources.insert(Camera::new(map_builder.player_start));
-            self.resources.insert(TurnState::AwaitingInput);
+            self.reset_game_state();
         };
+    }
+
+    fn victory(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, GREEN, BLACK, "Your have won.");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "You put on the Amulet of YALA and feel its power course through​ your veins.",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "Your town is saved and you can return to your normal life.",
+        );
+        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to play again.");
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.reset_game_state();
+        };
+    }
+
+    fn reset_game_state(&mut self) {
+        self.ecs = World::default();
+        self.resources = Resources::default();
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
+        spawn_player(&mut self.ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+        self.resources.insert(map_builder.map);
+        self.resources.insert(Camera::new(map_builder.player_start));
+        self.resources.insert(TurnState::AwaitingInput);
     }
 }
 
@@ -131,6 +158,9 @@ impl GameState for State {
             }
             TurnState::GameOver => {
                 self.game_over(ctx);
+            }
+            TurnState::Victory => {
+                self.victory(ctx);
             }
         }
         render_draw_buffer(ctx).expect("Render error");
--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -3,8 +3,12 @@ use crate::prelude::*;
 #[system]
 #[read_component(Health)]
 #[read_component(Player)]
+#[read_component(Point)]
+#[read_component(AmuletOfYala)]
 pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
-    let mut player_health = <&Health>::query().filter(component::<Player>());
+    let mut player_health = <(&Health, &Point)>::query().filter(component::<Player>());
+    let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
+
     let current_state = turn_state.clone();
 
     let mut new_state = match turn_state {
@@ -12,12 +16,19 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
         TurnState::PlayerTurn => TurnState::NpcsTurn,
         TurnState::NpcsTurn => TurnState::AwaitingInput,
         TurnState::GameOver => current_state,
+        TurnState::Victory => current_state,
     };
 
-    player_health.iter(ecs).for_each(|hp| {
+    let amulet_pos = amulet.iter(ecs).nth(0).unwrap();
+
+    player_health.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
             new_state = TurnState::GameOver;
         }
+        if pos == amulet_pos {
+            new_state = TurnState::Victory;
+        }
     });
+    
     *turn_state = new_state;
 }
